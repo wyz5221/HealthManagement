5b434c11bf746a498e67eb92463217f1869793a8
123 <15794286+N1Lin@user.noreply.gitee.com>
2025-07-04 09:26:56 +0800

    用户身体管理

---
+++ Health-server/src/main/java/com/jyx/healthsys/controller/BodyController.java
@@ -0,0 +1,16 @@
+package com.jyx.healthsys.controller;
+
+import com.jyx.Data_unification.Unification;
+import com.jyx.healthsys.entity.Body;
+import com.jyx.healthsys.mapper.BodyMapper;
+import com.jyx.healthsys.service.IBodyService;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/body")
+public class BodyController {
+
+}
+
+
